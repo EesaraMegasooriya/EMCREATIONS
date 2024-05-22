0e0a372797d9c6c20b642af3b3230cffb8b1a504
Eesara Megasooriya <165908029+EesaraMegasooriya@users.noreply.github.com>
2024-05-22 15:24:14 +0530

    Added Animation to home

--- emcreations/Frontend/Home.jsx
+++ emcreations/Frontend/Home.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import "./Home.css"
+import {useTypewriter, Cursor} from 'react-simple-typewriter';
 
 function Home() {
+  const [text] = useTypewriter({
+  words: ['Youtuber', 'Devoloper', 'Designer', 'Content Creator', 'Undergraduate'],
+  loop: {},
+    typeSpeed:120,
+    deleteSpeed:80,
+  });
+
+
   return (
-    <div>Home</div>
+    <div>
+
+      <div className="homemainanimcon">
+        
+      <div className="hommaintxt">Hello,
+        It's Eesara.</div>
+      <div className="animhomtxtcon">
+        <div className="animhomtxtmain">
+        I'm a {' '}
+        <span style={{fontWeight:'bold' , color: '#0091ff'}}>{text} </span>
+        <span style={{color:'red'}}><Cursor cursorStyle='|'/></span>
+        
+        </div>
+      </div>
+
+      </div>
+
+
+    </div>
   )
 }
 
