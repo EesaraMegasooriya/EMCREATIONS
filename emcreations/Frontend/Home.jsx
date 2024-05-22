c147e01320c70304e2fbd26c7ef6fecc8b51fc6e
Eesara Megasooriya <165908029+EesaraMegasooriya@users.noreply.github.com>
2024-05-22 19:31:01 +0530

    Img Added

--- emcreations/Frontend/Home.jsx
+++ emcreations/Frontend/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./Home.css"
 import {useTypewriter, Cursor} from 'react-simple-typewriter';
-import Mypic from '../src/assets/me.jpg'
+import Mypic from './me.jpg'
 
 function Home() {
   const [text] = useTypewriter({
