3615fe1ce45a79eeb2ecaf075dda9c0eb7ab15eb
Eesara Megasooriya <165908029+EesaraMegasooriya@users.noreply.github.com>
2024-05-26 00:43:08 +0530

    Added Header Menu

--- emcreations/Frontend/Header.jsx
+++ emcreations/Frontend/Header.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import './Header.css'
-import Logo from '../src/assets/E-tra.png'
+import React, { useState } from 'react';
+import './Header.css';
+import Logo from '../src/assets/E-tra.png';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div>
-<div className="headermaincon">
-    <div className="headerlogo">
-      <img src={Logo} className='Headerlogoclass'></img>
+      <div className="headermaincon">
+        <div className="headerlogo">
+          <img src={Logo} className='Headerlogoclass' alt="Logo" />
+        </div>
+        <div className="Headermaintextlogo">EMCREATIONS</div>
+        <div className="Headerrightside">
+          <a href="#home">Home</a>
+          <a href="#about">About Us</a>
+        </div>
+        <div className="menu-icon" onClick={toggleMenu}>
+          ☰
+        </div>
+      </div>
+      <div className={`slide-menu ${menuOpen ? 'open' : ''}`}>
+        <a href="#home" onClick={toggleMenu}>Home</a>
+        <a href="#about" onClick={toggleMenu}>About Us</a>
+      </div>
     </div>
-    <div className="Headermaintextlogo">EMCREATIONS</div>
-    <div className="Headerrightside">
-        Home
-        AboutUs
-    </div>
-   
-</div>
-
-
-    </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
