fa41bc193148327291792b20edabd24c28333c63
Eesara Megasooriya <165908029+EesaraMegasooriya@users.noreply.github.com>
2024-05-25 23:45:16 +0530

    Add Resposiveness for my projects

--- emcreations/Frontend/MyProjects.jsx
+++ emcreations/Frontend/MyProjects.jsx
@@ -4,23 +4,27 @@ import './MyProjects.css'
 const projects = [
   {
     title: 'WELLWORN',
-    description: 'Description of Project 1',
+    description: "This is a group project for an online fashion store, developed for a real client using the MERN stack. It showcases our team's collaborative and technical skills.",
     imageUrl: 'https://i.ibb.co/BtWNJnD/Untitled-design.png',
+    link: 'https://wellworn.example.com'
   },
   {
     title: 'BOBBY',
-    description: 'Description of Project 2',
+    description: 'This is my first Android game, created in Kotlin, to learn user input responsiveness and data storage. It offers an interactive and educational experience.',
     imageUrl: 'https://i.ibb.co/TPS1gtP/Thiya.png',
+    link: 'https://bobby.example.com'
   },
   {
     title: 'TeddyNotes',
     description: 'This Note app, implemented using Kotlin, is designed to help users learn and practice CRUD operations within a Kotlin application.',
     imageUrl: 'https://i.ibb.co/YWw44PZ/Screenshot-20240525-231520.png',
+    link: 'https://teddynotes.example.com'
   },
   {
     title: 'Travel Me',
     description: 'This Travel app, coded in Kotlin, is designed to explore modern UI/UX principles, offering an intuitive user experience.',
     imageUrl: 'https://i.ibb.co/Xfnsw9T/Screenshot-20240525-231039.png',
+    link: 'https://travelme.example.com'
   },
   // Add more projects here
 ]
@@ -36,6 +40,9 @@ function MyProjects() {
             <div className="project-title">{project.title}</div>
             <div className="project-overlay">
               <div className="project-description">{project.description}</div>
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
+                <button className="view-more-button">View More</button>
+              </a>
             </div>
           </div>
         ))}
